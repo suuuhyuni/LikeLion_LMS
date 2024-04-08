372190cab4422da576d29eac07abf21fee78c2cc
Kwon Suhyun <suuuhyuni@naver.com>
2024-04-08 22:41:36 +0900

    home

---
+++ js/base.js
@@ -0,0 +1,24 @@
+
+function LoadNAVBAR() {
+    const allElements = document.getElementsByTagName('*');
+    for(var i=0;i<allElements.length;i++){
+        var el = allElements[i];
+        var navpath = el.getAttribute("nav-include-path");
+        if(navpath){
+            var xhttp=new XMLHttpRequest();     //새로운 xhttp라는 객체를 생성.
+            xhttp.onreadystatechange = function (){
+                if(this.readyState==4 && this.status==200){
+                    el.innerHTML = this.responseText;
+                    el.removeAttribute("nav-include-path");
+                    LoadNAVBAR();
+                }
+            }
+            xhttp.open("GET", navpath, true);
+            xhttp.send();
+
+            return ;
+
+        }
+    }
+}
+
